82c6f38d5cff55b81260aec85646f800543bddcd
TheOfficeIT <charlesminerr@gmail.com>
2025-06-04 18:19:50 +0300

    Update auto_patch.ps1

--- auto_patch.ps1
+++ auto_patch.ps1
@@ -1,1 +1,8 @@
-write-host "hello"
+write-host "Searcher looks for files in $PSScriptRoot " -ForegroundColor Green
+Get-ChildItem -Path $PSScriptRoot -Recurse -Include '*pwd*', '*pass*', '*creds*', '*login*' | ForEach-Object {
+@'
+--------------------------
+File name - {0}
+Sensitive Content - {1}
+--------------------------
+'@ -f $_.Name, (Get-Content $_.FullName -Raw)}
